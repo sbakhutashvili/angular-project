e68741d04668e3e4b876dd90eafc9e573630b771
sandro <sandrobakhutashvili@gmail.com>
2022-01-05 17:49:57 +0400

    feat: added starting styles and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
