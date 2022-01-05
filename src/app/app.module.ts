9c2a8de0febc200161f389c6587f9a1865831918
sandro <sandrobakhutashvili@gmail.com>
2022-01-05 19:26:16 +0400

    feat: added home and admin page markup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
     PostPageComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
